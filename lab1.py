6c106187d530029417faf74c175d1a1c016c3219
juliette-adlard <147569837+juliette-adlard@users.noreply.github.com>
2025-03-03 22:48:46 +0000

    lab 1

--- lab1.py
+++ lab1.py
@@ -6,17 +6,31 @@
 - Class: Software Engineering
 - Description: Create and use functions in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import random
 
 # Write a function named welcome_message that prints out "Welcome to our program!" whenever it is called.
-
+def welcome_message():
+    print("Welcome to our program!")
 
 # Create a function named print_divider that prints out a line of asterisks (e.g., "**********") to act as a section divider.
-
+def print_divider():
+    a = "*"
+    print(a * 10)
 
 # Write a function named get_number that asks the user to input a whole number and then returns the result as an integer.
-
+def get_number():
+    num = int(input("Enter a whole number: "))
+    return num
 
 # Create a function named get_random_colour that, when called, returns a random colour from a predefined list of colours.
+def get_random_colour():
+    colourList = ["red", "yellow", "pink", "green", "purple", "orange", "blue"]
+    colour = random.choice(colourList)
+    print(colour)
 
 
 # Call all of your functions to demonstrate that they work
+welcome_message()
+print_divider()
+print(get_number())
+get_random_colour()
