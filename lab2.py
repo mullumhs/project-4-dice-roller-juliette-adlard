e8ac10eb3a80f28c217fcbbfc13d96b34252cb65
juliette-adlard <147569837+juliette-adlard@users.noreply.github.com>
2025-03-12 22:49:09 +0000

    lab 2

--- lab2.py
+++ lab2.py
@@ -8,15 +8,32 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a function named say_hello that accepts a person's name as a parameter and prints "Hello" followed by the name.
-
+def say_hello(name):
+    print(f"Hello {name}.")
 
 # Develop a function named triple that takes one number as a parameter and returns the number multiplied by three.
-
+def triple(x):
+    y = x * 3
+    return y
 
 # Write a function called add that takes two numbers as parameters and returns their sum. 
-
+def add(x, y):
+    sum = x + y
+    return sum
 
 # Create a function named draw_line that takes one parameter for the length of the line and prints a straight line of that many hyphens.
-
+def draw_line(length):
+    print("-" * length)
 
 # Call your functions, printing out the return result where appropriate
+name = input("Enter your name: ").title()
+say_hello(name)
+x = float(input("Enter a number: "))
+y = triple(x)
+print(f"Triple your number is {y}.")
+x = float(input("Enter a number: "))
+y = float(input("Enter a number: "))
+sum = add(x, y)
+print(f"The sum of your two numbers is {sum}")
+length = int(input("Enter a number to determine the length of your line: "))
+draw_line(length)
--- lab3.py
+++ lab3.py
@@ -9,12 +9,21 @@
 
 # Define a function called get_number(prompt) that returns an integer. 
 # Include a while loop and try/except blocks inside the function to handle non-integer inputs.
-
+def get_number():
+    while True:
+        try:
+            num = int(input("Enter an integer: "))
+            return num
+        except:
+            print("Invalid input. An integer is a whole number.")
 
 
 # Use the get_number function to ask for a numerator, then again for a denominator.
 # Divide the numerator by the denominator and print the result, handling any division by zero errors.
-
+n = get_number()
+d = get_number()
+quotient = n/d
+print(f"{n} divided by {d} is {quotient}")
 
 
 # Use the get_number function to ask the user for an index to access an element from a predefined list. 
